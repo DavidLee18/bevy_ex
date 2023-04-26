a5a40ca58f907839d6c012ea59c7724b25e26b39
David Lee <djwodus@gmail.com>
2023-04-27 02:28:26 +0900

    setting up a fly cam

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,28 @@
-pub mod events;
-mod game;
-mod main_menu;
-mod systems;
-
-use bevy::{prelude::*, log::LogPlugin};
-use game::GamePlugin;
-use main_menu::MainMenuPlugin;
-use systems::*;
+use bevy::prelude::*;
+use bevy_flycam::prelude::*;
 
 fn main() {
     App::new()
-    .add_plugins(DefaultPlugins.set(LogPlugin {
-        ..Default::default()
-    }))
-    .add_state::<AppState>()
-    .add_plugin(MainMenuPlugin)
-    .add_plugin(GamePlugin)
-    .add_startup_system(spawn_camera)
-    .add_systems((
-        transition_to_state(KeyCode::G, AppState::Game),
-        transition_to_state(KeyCode::M, AppState::MainMenu),
-        exit_game,
-        handle_game_over
-    ))
-    .run()
+    .add_plugins(DefaultPlugins)
+    .add_plugin(NoCameraPlayerPlugin)
+    .add_startup_systems((setup_cam, spawn_cubes))
+    .run();
 }
 
-#[derive(States, PartialEq, Eq, Clone, Copy, Default, Debug, Hash)]
-pub enum AppState {
-    #[default] MainMenu,
-    Game,
-    GameOver
+fn setup_cam(mut commands: Commands) {
+    commands.spawn((Camera3dBundle::default(), FlyCam));
+}
+
+fn spawn_cubes(mut commands: Commands, mut mesh_assets: ResMut<Assets<Mesh>>) {
+    let mesh = mesh_assets.add(shape::Box::new(1.0, 1.0, 1.0).into());
+
+    for x in -10..10 {
+        for z in -10..10 {
+            commands.spawn(PbrBundle {
+                mesh: mesh.clone(),
+                transform: Transform::from_xyz(x as f32 * 2.0, 0.0, z as f32 * 2.0),
+                ..Default::default()
+            });
+        }
+    }
 }
